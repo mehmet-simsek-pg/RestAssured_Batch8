9b5ce2ce5cbdeb9b85055fe2093a28e3c0c071b8
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-11-13 18:46:39 +0100

    All students endpoint tests added

--- src/test/java/api/StudentTests.java
+++ src/test/java/api/StudentTests.java
@@ -9,7 +9,10 @@ import static org.hamcrest.Matchers.notNullValue;
 
 public class StudentTests extends BaseTest{
 
-    @Test
+    private int studentID; // bu degiskeni ancak tüm testleri
+    // ayni anda calistirirsak kullanbiliriz.
+
+    @Test(priority = 1, description = "Create a new student")
     public void createStudent() {
 
         String createBody = """
@@ -44,5 +47,78 @@ public class StudentTests extends BaseTest{
         // response nesnesinden path() methodu ile response icerisindeki degerlere ulasabilirim.
         // key-value mantiginda
         System.out.println("Create edilen student id: " + response.path("id"));
+        studentID = response.path("id");
     }
+
+    @Test(priority = 2, description = "Get all students infos")
+    public void getAllStudents() {
+
+        Response response = // response body e ihtiyacim oldugu icin nesne ürettik
+        given()
+                .spec(request)
+
+                .when()
+                .get("/students")
+
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        // Response body i ekrana yazdirdik
+        response.body().prettyPrint();
+    }
+
+    @Test(priority = 3, description = "Get student data by id")
+    public void getStudentById() {
+
+        Response response =
+                given()
+                        .spec(request)
+
+                        .when()
+                        .get("/students/{id}", studentID)
+
+                        .then()
+                        .statusCode(200)
+                        .extract().response();
+
+        response.body().prettyPrint();
+    }
+
+    @Test(priority = 4, description = "Update student data")
+    public void updateStudent() {
+
+        String updateBody = """
+                {
+                  "firstName": "Ali",
+                  "lastName": "Güncellendi",
+                  "email": "ali.updated@example.com"
+                }
+                """;
+
+        given()
+                .spec(request)
+                .body(updateBody)
+
+                .when()
+                .put("/students/{id}", studentID)
+
+                .then()
+                .statusCode(200);
+    }
+
+    @Test(priority = 5, description = "Delete existing student")
+    public void deleteStudent() {
+
+        given()
+                .spec(request)
+
+                .when()
+                .delete("/students/{id}", studentID)
+
+                .then()
+                .statusCode(204);
+
+    }
+
 }
