1987dd26b9af17f75d4760a22fc25a5d124cbdd1
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-11-13 17:20:16 +0100

    base test created

--- src/test/java/api/BaseTest.java
+++ src/test/java/api/BaseTest.java
@@ -1,4 +1,20 @@
 package api;
 
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+
 public class BaseTest {
+
+    protected RequestSpecification request;
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://demo-hn2f.onrender.com";
+
+        request = RestAssured
+                .given()
+                .contentType("application/json")
+                .accept("application/json");
+    }
 }
